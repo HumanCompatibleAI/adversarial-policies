041d009d1e974d44a31236570df42895fa544e30
Adam Gleave <adam@gleave.me>
2018-11-19 20:18:40 -0800

    Register environments

--- aprl/envs/__init__.py
+++ aprl/envs/__init__.py
@@ -1,2 +1,18 @@
+from gym.envs.registration import register
+
 from aprl.envs.multi_agent import MultiAgentEnv
-from aprl.envs.matrix_game import MatrixGame, IteratedMatchingPennies, RockPaperScissors
+from aprl.envs.matrix_game import IteratedMatchingPennies, RockPaperScissors
+
+register(
+    id='aprl/IMP-v0',
+    entry_point='aprl.envs.matrix_game:IteratedMatchingPennies',
+    max_episode_steps=200,
+    reward_threshold=100,
+)
+
+register(
+    id='aprl/RPS-v0',
+    entry_point='aprl.envs.matrix_game:RockPaperScissors',
+    max_episode_steps=200,
+    reward_threshold=100,
+)
