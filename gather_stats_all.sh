7ade244d029338c3e4b274a4719a673aebe477f0
Michael Dennis <michael@astar.Banatao.Berkeley.EDU>
2018-12-12 12:05:36 -0800

    added some files I have been using to run experiments

--- gather_stats_all.sh
+++ gather_stats_all.sh
@@ -1,5 +1,2 @@
 
-for file in $1/*
-do
-	  python gather_statistics.py --samples $2 --nearly_silent True --no_visuals True --agent_to_eval "$file" --agent_type our_mlp |grep '^\[MAGIC NUMBER 87623123]' | tee -a "all_stats.log" 
-done
+parallel --delay 1 --load 100% --noswap --results logs/ --header : bash single_gather_stats.sh {file} {num} $3 ::: file $1/* ::: num $2 
