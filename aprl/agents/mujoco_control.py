9cb05132f91a145cea90280cfeca957592c287f4
Adam Gleave <adam@gleave.me>
2019-01-26 18:55:36 -0800

    MuJoCo control: finite difference on qacc like derivative.cpp

--- aprl/agents/mujoco_control.py
+++ aprl/agents/mujoco_control.py
@@ -3,12 +3,14 @@ https://github.com/anassinator/ilqr. Specifically, implements
 finite-difference approximations for dynamics and cost."""
 
 from collections import namedtuple
+from enum import Enum
 from functools import reduce
 
 from ilqr.cost import FiniteDiffCost
-from ilqr.dynamics import FiniteDiffDynamics
-from mujoco_py import MjSimState
+from ilqr.dynamics import Dynamics, FiniteDiffDynamics
+from mujoco_py import MjSimState, functions as mjfunc
 import numpy as np
+from scipy.optimize import approx_fprime
 
 from aprl.utils import getattr_unwrapped
 
@@ -77,9 +79,9 @@ class MujocoFiniteDiffCost(MujocoFiniteDiff, FiniteDiffCost):
 # Once you know what fields you actually care about, get rid of the dynamic logic.
 class MujocoRelevantState(namedtuple('MujocoStateBase',
         'qpos qvel qacc qacc_warmstart qfrc_applied xfrc_applied')):
-    '''Based off MjSimState'''
+    """Based off MjSimState"""
     def flatten(self):
-        """ Flattens a state into a numpy array of numbers."""
+        """Flattens a state into a numpy array of numbers."""
         res = []
         for f in self._fields:
             v = getattr(self, f)
@@ -198,68 +200,199 @@ class MujocoFiniteDiffDynamicsLowLevel(MujocoFiniteDiff, FiniteDiffDynamics):
 # cannot easily swap out for forward. I'm a bit confused why they do
 # frame skip rather than changing dt. I guess smaller dt makes better physics sim?
 
-class MujocoFiniteDiffDynamicsWarmstart(MujocoFiniteDiff, FiniteDiffDynamics):
-    NWARMUP = 4
-    FIELDS = ['qpos', 'qvel']
+#TODO: Cythonize
+class MujocoPerformanceState(namedtuple('MujocoPerformanceStateBase', 'qpos qvel')):
+    """Represents state from the MuJoCo simulator needed for planning,
+       namely position and velocity."""
 
+    @staticmethod
+    def from_mjdata(data):
+        return MujocoPerformanceState(data.qpos, data.qvel)
+
+    @staticmethod
+    def from_flattened(flattened, sim):
+        qpos = flattened[0:sim.model.nq]
+        qvel = flattened[sim.model.nq:sim.model.nq + sim.model.nv]
+        return MujocoPerformanceState(qpos, qvel)
+
+    def set_mjdata(self, data):
+        data.qpos[:] = self.qpos
+        data.qvel[:] = self.qvel
+
+    def flatten(self):
+        return np.concatenate((self. qpos, self.qvel))
+
+
+class SkipStages(Enum):
+    NONE = 0
+    POS = 1
+    VEL = 2
+    ACC = 3
+
+
+def finite_diff(sim, xptr, qacc_warmstart, skip, eps):
+    #TODO: do positions in quaternion space
+    #TODO: use qacc rather than qvel & qpos?
+    orig = np.copy(xptr)
+    center = np.copy(sim.data.qacc)
+    nin = len(orig)
+    nout = len(center)
+    jacobian = np.zeros((nout, nin), dtype=np.float64)
+    for j in range(nin):
+        xptr[j] += eps
+        sim.data.qacc_warmstart[:] = qacc_warmstart
+        mjfunc.mj_forwardSkip(sim.model, sim.data, skip.value, 1)
+        jacobian[:, j] = (sim.data.qacc - center) / eps
+        xptr[j] = orig[j]
+    return jacobian
+
+
+class MujocoFiniteDiffDynamicsPerformance(MujocoFiniteDiff, Dynamics):
+    """Finite difference dynamics for a MuJoCo environment."""
+    NWARMUP = 4
+    #TODO: assume u to ctrl mapping?
+
+    #TODO: assumes frame skip is 1. Can we generalize? Do we want to?
     def __init__(self, env, x_eps=1e-6, u_eps=1e-6):
+        """
+        :param env (MujocoEnv): a Gym MuJoCo environment. Must not be wrapped.
+               CAUTION: assumes env.step(u) sets MuJoCo ctrl to u. (This is true
+               for all the built-in Gym MuJoCo environments as of 2019-01.)
+        :param x_eps (float): increment to use for finite difference on state.
+        :param u_eps (float): increment to use for finite difference on control.
+        """
         MujocoFiniteDiff.__init__(self, env)
-        FiniteDiffDynamics.__init__(self, self._mujoco_f,
-                                    self.state_size, self.action_size,
-                                    x_eps=x_eps, u_eps=u_eps)
-        self.qacc_warmstart = np.zeros_like(self.sim.data.qacc_warmstart)
+        Dynamics.__init__(self)
+        self._x_eps = x_eps
+        self._u_eps = u_eps
+        self.warmstart = np.zeros_like(self.sim.data.qacc_warmstart)
         self.warmstart_for = None
 
-    def _warmstart(self, x):
-        if np.array_equal(self.warmstart_for, x):
+    @property
+    def state_size(self):
+        """State size."""
+        return self._state_size
+
+    @property
+    def action_size(self):
+        """Action size."""
+        return self._action_size
+
+    def _warmstart(self):
+        """Calculate qacc_warmstart. Initializing qacc_warmstart in the
+           simulator will result in a more rapid computation of qacc when
+           other fields (e.g. qpos, qvel, ctrl) are only slightly changed."""
+        # TODO: This is largely undocumented in MuJoCo; I'm following the lead
+        # of derivative.cpp. Does this have any implications for correctness,
+        # or just performance?
+
+        curr_state = np.concatenate((self.get_state(), self.sim.data.ctrl))
+        if np.array_equal(self.warmstart_for, curr_state):
+            # self.qacc_warmstart has already been computed for this x.
             return
-        self.set_state(x)
+
+        self.sim.forward()  # run full computation
         for i in range(self.NWARMUP):
-            # TODO: should be skip stage velocity
-            # I think this is just a performance improvement, but may have
-            # correctness implications. mujoco_py doesn't forwardSkip publicly,
-            # but does have mj_forwardSkip defined internally.
-            self.sim.forward()
-        self.qacc_warmstart[:] = self.sim.data.qacc_warmstart
-        self.warmstart_for = x
+            # iterate several times to get a good qacc_warmstart
+            mjfunc.mj_forwardSkip(self.env.sim.model, self.env.sim.data,
+                                  SkipStages.VEL.value, 1)
+        self.warmstart[:] = self.sim.data.qacc_warmstart
+        self.warmstart_for = curr_state
 
     def f(self, x, u, i):
-        self._warmstart(x)
-        return self._mujoco_f(x, u, i)
+        """Dynamics model. See FiniteDiffDynamics."""
+        self.set_state(x)
+        self.sim.data.ctrl[:] = u
+        self._warmstart()
+        self.sim.step()
+        return self.get_state()
 
     def f_x(self, x, u, i):
+        """Partial derivative of dynamics model w.r.t. to x.
+           See FiniteDiffDynamics.
+
+           Assumes that the control input u is mapped into raw control ctrl
+           in the simulator by a function that does not depend on x."""
         #TODO: performance improvement -- can call step just once to get ctrl
         #then just do F.D. on x with sim forward/step. (Only makes sense if frameskip is one.)
-        self._warmstart(x)
-        return super().f_x(x, u, i)
+
+        self.set_state(x)
+        self.sim.data.ctrl[:] = u
+        self._warmstart()
+        self.sim.forward()
+
+        # Finite difference over velocity: skip = POS
+        jacob_vel = finite_diff(self.sim, self.sim.data.qvel, self.warmstart,
+                                SkipStages.POS, self._x_eps)
+        jacob_pos = finite_diff(self.sim, self.sim.data.qpos, self.warmstart,
+                                SkipStages.NONE, self._x_eps)
+        # This Jacobian is qacc differentiated w.r.t. (qpos, qvel).
+        acc_jacobian = np.concatenate((jacob_pos, jacob_vel), axis=1)
+        # But we want (qpos, qvel) differentiated w.r.t. (qpos, qvel).
+        # x_{t+1} = x_t + dt * \dot{x}_t
+        # \dot{x}_{t+1} = \dot{x}_t + \ddot{x}_t
+        # \nabla_{(x_t, \dot{x}_t)} \ddot{x}_t = acc_jacobian
+        # So \nabla_{(x_t, \dot{x}_t)} \dot{x}_t = (
+        #      dt * \nabla_{x_t} \ddot{x}_t,
+        #      I + dt * \nabla_{\dot{x}_t} \ddot{x}_t)
+        # And \nabla_{(x_t, \dot{x}_t)} x_t = (I, dt * I)
+        # TODO: Could use an alternative linearization?
+        dt = self.sim.model.opt.timestep
+        A = np.eye(self.sim.model.nq)
+        B = dt * np.eye(self.sim.model.nv)
+        C = dt * acc_jacobian[:, :self.sim.model.nq]
+        D = np.eye(self.sim.model.nv) + dt * acc_jacobian[:, self.sim.model.nq:]
+        state_jacobian = np.vstack([
+            np.hstack([
+                A,
+                B,
+            ]),
+            np.hstack([
+                C,
+                D,
+            ])
+        ])
+
+        return state_jacobian
 
     def f_u(self, x, u, i):
-        self._warmstart(x)
-        return super().f_u(x, u, i)
+        self.set_state(x)
+        self.sim.data.ctrl[:] = u
+        self._warmstart()
+        self.sim.forward()
+
+        # Finite difference over control: skip = VEL
+        # This Jacobian is qacc differentiated w.r.t. ctrl.
+        jacobian_acc = finite_diff(self.sim, self.sim.data.ctrl, self.warmstart,
+                               SkipStages.VEL, self._u_eps)
+        # But we want (qpos, qvel) differentiated w.r.t. ctrl.
+        # \nabla_{u_t} \ddot{x}_t = acc_jacobian
+        # \nabla_{u_t} \dot{x}_t = dt * \nabla_{u_t} \ddot{x}_t
+        # \nabla_{u_t} x_t = 0
+        # TODO: Could use an alternative linearization?
+        dt = self.sim.model.opt.timestep
+        state_jacobian = np.vstack([
+            np.zeros((self.sim.model.nq, self.sim.model.nu)),
+            dt * jacobian_acc
+        ])
+        return state_jacobian
 
     def f_xx(self, x, u, i):
-        self._warmstart(x)
-        return super().f_xx(x, u, i)
+        raise NotImplementedError()
 
     def f_ux(self, x, u, i):
-        self._warmstart(x)
-        return super().f_ux(x, u, i)
+        raise NotImplementedError()
 
     def f_uu(self, x, u, i):
-        self._warmstart(x)
-        return super().f_uu(x, u, i)
+        raise NotImplementedError()
+
+    @property
+    def has_hessians(self):
+        return False
 
     def get_state(self):
-        return MujocoRelevantState.from_mjdata(self.sim.data, self.FIELDS).flatten()
+        return MujocoPerformanceState.from_mjdata(self.sim.data).flatten()
 
     def set_state(self, x):
-        state = MujocoRelevantState.from_flattened(x, self.sim)
+        state = MujocoPerformanceState.from_flattened(x, self.sim)
         state.set_mjdata(self.sim.data)
-        self.sim.forward()  # put mjData in consistent state
-
-    def _mujoco_f(self, x, u, i):
-        #TODO: something more efficient, also figure out if step vs forward breaks things
-        self.set_state(x)
-        self.sim.data.qacc_warmstart[:] = self.qacc_warmstart
-        self.env.step(u)  # evaluated for side-effects on state
-        return self.get_state()
