5198eb1268afb8dbc7292b390c0db8ba79d4586a
Adam Gleave <adam@gleave.me>
2019-01-26 19:48:57 -0800

    MuJoCo control: code cleanup

--- aprl/agents/__init__.py
+++ aprl/agents/__init__.py
@@ -1,4 +1,5 @@
 from aprl.agents.multi_monitor import MultiMonitor
 from aprl.agents.self_play import SelfPlay
 from aprl.agents.ppo_self_play import PPOSelfPlay
-from aprl.agents.mujoco_control import MujocoFiniteDiffDynamics, MujocoFiniteDiffCost
+from aprl.agents.mujoco_control import MujocoFiniteDiffCost, \
+    MujocoFiniteDiffDynamicsBasic, MujocoFiniteDiffDynamicsPerformance
--- aprl/agents/mujoco_control.py
+++ aprl/agents/mujoco_control.py
@@ -8,11 +8,33 @@ from functools import reduce
 
 from ilqr.cost import FiniteDiffCost
 from ilqr.dynamics import Dynamics, FiniteDiffDynamics
-from mujoco_py import MjSimState, functions as mjfunc
+from mujoco_py import functions as mjfunc
 import numpy as np
-from scipy.optimize import approx_fprime
 
 from aprl.utils import getattr_unwrapped
+
+#TODO: Cythonize
+class MujocoState(namedtuple('MujocoStateBase', 'qpos qvel')):
+    """Represents state from the MuJoCo simulator needed for planning,
+       namely position and velocity."""
+
+    @staticmethod
+    def from_mjdata(data):
+        return MujocoState(data.qpos, data.qvel)
+
+    @staticmethod
+    def from_flattened(flattened, sim):
+        qpos = flattened[0:sim.model.nq]
+        qvel = flattened[sim.model.nq:sim.model.nq + sim.model.nv]
+        return MujocoState(qpos, qvel)
+
+    def set_mjdata(self, data):
+        data.qpos[:] = self.qpos
+        data.qvel[:] = self.qvel
+
+    def flatten(self):
+        return np.concatenate((self. qpos, self.qvel))
+
 
 class MujocoFiniteDiff(object):
     def __init__(self, env):
@@ -23,35 +45,17 @@ class MujocoFiniteDiff(object):
             raise TypeError("Environment must not have a timestep limit.")
         self.env = env
         self.sim = getattr_unwrapped(env, 'sim')
-        state = self.get_state()
+        state = MujocoState.from_mjdata(self.sim.data).flatten()
         self._state_size = len(state)
         self._action_size = reduce(lambda x, y : x * y, env.action_space.shape)
 
     def get_state(self):
-        return self.sim.get_state().flatten()
+        return MujocoState.from_mjdata(self.sim.data).flatten()
 
     def set_state(self, x):
-        state = MjSimState.from_flattened(x, self.sim)
-        self.sim.set_state(state)
+        state = MujocoState.from_flattened(x, self.sim)
+        state.set_mjdata(self.sim.data)
         self.sim.forward()  # put mjData in consistent state
-
-
-class MujocoFiniteDiffDynamics(MujocoFiniteDiff, FiniteDiffDynamics):
-    """Finite difference approximation to dynamics of a MujocoEnv.
-    WARNING: assumes environment has no termination condition -- will ignore
-    step returning done. (It's not clear what the correct thing to do here is.)
-    """
-
-    def __init__(self, env, x_eps=1e-6, u_eps=1e-6):
-        MujocoFiniteDiff.__init__(self, env)
-        FiniteDiffDynamics.__init__(self, self._mujoco_f,
-                                    self.state_size, self.action_size,
-                                    x_eps=x_eps, u_eps=u_eps)
-
-    def _mujoco_f(self, x, u, i):
-        self.set_state(x)
-        self.env.step(u)  # evaluated for side-effects on state
-        return self.get_state()
 
 
 class MujocoFiniteDiffCost(MujocoFiniteDiff, FiniteDiffCost):
@@ -75,152 +79,33 @@ class MujocoFiniteDiffCost(MujocoFiniteDiff, FiniteDiffCost):
         to override this on a case-by-case basis."""
         return 0
 
-# TODO: This is fairly inefficient, and ugly to boot.
-# Once you know what fields you actually care about, get rid of the dynamic logic.
-class MujocoRelevantState(namedtuple('MujocoStateBase',
-        'qpos qvel qacc qacc_warmstart qfrc_applied xfrc_applied')):
-    """Based off MjSimState"""
-    def flatten(self):
-        """Flattens a state into a numpy array of numbers."""
-        res = []
-        for f in self._fields:
-            v = getattr(self, f)
-            if v is not None:
-                res.append(v.flatten())
-        return np.concatenate(res)
 
-    @staticmethod
-    def from_flattened(a, sim):
-        len_a = len(a)
-        idx_qpos = 0
-        idx_qvel = idx_qpos + sim.model.nq
-        idx_qacc = idx_qvel + sim.model.nv
-        idx_qacc_warmstart = idx_qacc + sim.model.nv
-        idx_qfrc_applied = idx_qacc_warmstart + sim.model.nv
-        idx_xfrc_applied = idx_qfrc_applied + sim.model.nv
+class MujocoFiniteDiffDynamicsBasic(MujocoFiniteDiff, FiniteDiffDynamics):
+    """Computes finite difference approximation to dynamics of a MujocoEnv.
 
-        qpos = a[idx_qpos:idx_qpos + sim.model.nq]
+    Uses Gym step() directly to advance the simulation and exert control.
+    However, does directly access MuJoCo qpos and qvel to use as an state,
+    instead of Gym observations.
 
-        qvel = None
-        if idx_qvel < len_a:
-            qvel = a[idx_qvel:idx_qvel + sim.model.nv]
+    WARNING: assumes environment has no termination condition -- will ignore
+    step returning done. (It's not clear what the correct thing to do here is.)
+    """
+    def __init__(self, env, x_eps=1e-6, u_eps=1e-6):
+        """Inits MujocoFiniteDiffDynamicsBase with a MujocoEnv.
 
-        qacc = None
-        if idx_qacc < len_a:
-            qacc = a[idx_qacc:idx_qacc + sim.model.nv]
-
-        qacc_warmstart = None
-        if idx_qacc_warmstart < len_a:
-            qacc_warmstart = a[idx_qacc_warmstart:idx_qacc_warmstart + sim.model.nv]
-
-        qfrc_applied = None
-        if idx_qfrc_applied < len_a:
-            qfrc_applied = a[idx_qfrc_applied:idx_qfrc_applied + sim.model.nv]
-
-        xfrc_applied = None
-        if idx_xfrc_applied < len_a:
-            xfrc_applied = a[idx_xfrc_applied:idx_xfrc_applied + sim.model.nbody * 6]
-            xfrc_applied = xfrc_applied.reshape(sim.model.nbody, 6)
-
-        return MujocoRelevantState(qpos, qvel,
-                                   qacc, qacc_warmstart,
-                                   qfrc_applied, xfrc_applied)
-
-    @classmethod
-    def from_mjdata(cls, data, fields=None):
-        if fields is None:
-            fields = cls._fields
-        kwargs = {f: getattr(data, f) for f in fields}
-        return MujocoRelevantState(**kwargs)
-
-    def set_mjdata(self, data):
-        for k in self._fields:
-            assert hasattr(data, k)
-            v = getattr(self, k)
-            if v is not None:
-                getattr(data, k)[:] = v
-MujocoRelevantState.__new__.__defaults__ = (None,) * 6
-
-
-class MujocoFiniteDiffDynamicsLowLevel(MujocoFiniteDiff, FiniteDiffDynamics):
-    def __init__(self, env, x_eps=1e-6, u_eps=1e-6, kind='recommended'):
-        if kind == 'all':
-            self.fields = None
-        elif kind == 'basic':
-            self.fields = ['qpos', 'qvel']
-        elif kind == 'basic_plus':
-            self.fields = ['qpos', 'qvel', 'qacc']
-        elif kind == 'recommended':
-            self.fields = ['qpos', 'qvel', 'qacc', 'qacc_warmstart']
-        else:
-            raise ValueError("Unrecognised kind: '{}'".format(kind))
-
+        :param env (MujocoEnv): a Gym MuJoCo environment. Must not be wrapped.
+        :param x_eps (float): increment to use for finite difference on state.
+        :param u_eps (float): increment to use for finite difference on control.
+        """
         MujocoFiniteDiff.__init__(self, env)
         FiniteDiffDynamics.__init__(self, self._mujoco_f,
                                     self.state_size, self.action_size,
                                     x_eps=x_eps, u_eps=u_eps)
 
-    def get_state(self):
-        return MujocoRelevantState.from_mjdata(self.sim.data, self.fields).flatten()
-
-    def set_state(self, x):
-        state = MujocoRelevantState.from_flattened(x, self.sim)
-        state.set_mjdata(self.sim.data)
-        self.sim.forward()  # put mjData in consistent state
-
     def _mujoco_f(self, x, u, i):
-        #TODO: something more efficient, also figure out if step vs forward breaks things
         self.set_state(x)
         self.env.step(u)  # evaluated for side-effects on state
         return self.get_state()
-
-
-# derivative.cpp copies from dmain:
-# qpos; qvel; qacc; qacc_warmstart; qfrc_applied; xfrc_applied; ctrl
-# Does one forward step, and nwarmup forwardSkip(mjSTAGE_VEL).
-# For each differencing step:
-# + Copies warmstart; does forwardSkip with appropriate stage.
-# + Everything else is stage-specific.
-# + For perturbations in qfrc_applied, only needs to restore qacc. Since forwardSkip does not integrate, it doesn't change the rest of the state.
-# + For perturbations in qvel, have to restore qvel; otherwise same.
-# + For perturbations in position, have to restore qpos. They also do different perturbations depending on joint type (to get consistent eps, I think).
-# I think the parts important for correctness are:
-# + Copying all the relevant bits from dmain. Done, I think (except ctrl, but you set that.)
-# + Warmstarting.
-# + Angle perturbation. I'm not directly changing ctrl, but rather action in env.step, so hard to normalize in general. I'd be a bit surprised if this really broke things, though.
-# Rest is for efficiency, including using forward rather than skip.
-# So probably go for correctness first. Then figure out how to make it efficient.
-# (May want to just use their C++ code, if you can factor MuJoCo v.s. policy FD.)
-
-# MjSimState: time qpos qvel act udd_state
-# It's ok to omit qfrc_applied and xfrc_applied since Gym never sets these,
-# just using the ctrl field.
-
-# Gym environments use frame-skip (2 for Reacher, 5 for HalfCheetah), so
-# cannot easily swap out for forward. I'm a bit confused why they do
-# frame skip rather than changing dt. I guess smaller dt makes better physics sim?
-
-#TODO: Cythonize
-class MujocoPerformanceState(namedtuple('MujocoPerformanceStateBase', 'qpos qvel')):
-    """Represents state from the MuJoCo simulator needed for planning,
-       namely position and velocity."""
-
-    @staticmethod
-    def from_mjdata(data):
-        return MujocoPerformanceState(data.qpos, data.qvel)
-
-    @staticmethod
-    def from_flattened(flattened, sim):
-        qpos = flattened[0:sim.model.nq]
-        qvel = flattened[sim.model.nq:sim.model.nq + sim.model.nv]
-        return MujocoPerformanceState(qpos, qvel)
-
-    def set_mjdata(self, data):
-        data.qpos[:] = self.qpos
-        data.qvel[:] = self.qvel
-
-    def flatten(self):
-        return np.concatenate((self. qpos, self.qvel))
 
 
 class SkipStages(Enum):
@@ -230,9 +115,8 @@ class SkipStages(Enum):
     ACC = 3
 
 
-def finite_diff(sim, xptr, qacc_warmstart, skip, eps):
+def _finite_diff(sim, xptr, qacc_warmstart, skip, eps):
     #TODO: do positions in quaternion space
-    #TODO: use qacc rather than qvel & qpos?
     orig = np.copy(xptr)
     center = np.copy(sim.data.qacc)
     nin = len(orig)
@@ -249,12 +133,11 @@ def finite_diff(sim, xptr, qacc_warmstart, skip, eps):
 
 class MujocoFiniteDiffDynamicsPerformance(MujocoFiniteDiff, Dynamics):
     """Finite difference dynamics for a MuJoCo environment."""
-    NWARMUP = 4
-    #TODO: assume u to ctrl mapping?
-
+    NWARMUP = 3
     #TODO: assumes frame skip is 1. Can we generalize? Do we want to?
     def __init__(self, env, x_eps=1e-6, u_eps=1e-6):
-        """
+        """Inits MujocoFiniteDiffDynamicsPerformance with a MujocoEnv.
+
         :param env (MujocoEnv): a Gym MuJoCo environment. Must not be wrapped.
                CAUTION: assumes env.step(u) sets MuJoCo ctrl to u. (This is true
                for all the built-in Gym MuJoCo environments as of 2019-01.)
@@ -267,6 +150,11 @@ class MujocoFiniteDiffDynamicsPerformance(MujocoFiniteDiff, Dynamics):
         self._u_eps = u_eps
         self.warmstart = np.zeros_like(self.sim.data.qacc_warmstart)
         self.warmstart_for = None
+
+    @property
+    def has_hessians(self):
+        """See base class Dynamics."""
+        return False
 
     @property
     def state_size(self):
@@ -301,31 +189,25 @@ class MujocoFiniteDiffDynamicsPerformance(MujocoFiniteDiff, Dynamics):
 
     def f(self, x, u, i):
         """Dynamics model. See FiniteDiffDynamics."""
-        self.set_state(x)
-        self.sim.data.ctrl[:] = u
+        self._set_state_action(x, u)
         self._warmstart()
         self.sim.step()
         return self.get_state()
 
     def f_x(self, x, u, i):
         """Partial derivative of dynamics model w.r.t. to x.
-           See FiniteDiffDynamics.
+           See base class Dynamics.
 
            Assumes that the control input u is mapped into raw control ctrl
            in the simulator by a function that does not depend on x."""
-        #TODO: performance improvement -- can call step just once to get ctrl
-        #then just do F.D. on x with sim forward/step. (Only makes sense if frameskip is one.)
-
-        self.set_state(x)
-        self.sim.data.ctrl[:] = u
+        self._set_state_action(x, u)
         self._warmstart()
-        self.sim.forward()
 
         # Finite difference over velocity: skip = POS
-        jacob_vel = finite_diff(self.sim, self.sim.data.qvel, self.warmstart,
-                                SkipStages.POS, self._x_eps)
-        jacob_pos = finite_diff(self.sim, self.sim.data.qpos, self.warmstart,
-                                SkipStages.NONE, self._x_eps)
+        jacob_vel = _finite_diff(self.sim, self.sim.data.qvel, self.warmstart,
+                                 SkipStages.POS, self._x_eps)
+        jacob_pos = _finite_diff(self.sim, self.sim.data.qpos, self.warmstart,
+                                 SkipStages.NONE, self._x_eps)
         # This Jacobian is qacc differentiated w.r.t. (qpos, qvel).
         acc_jacobian = np.concatenate((jacob_pos, jacob_vel), axis=1)
         # But we want (qpos, qvel) differentiated w.r.t. (qpos, qvel).
@@ -343,28 +225,25 @@ class MujocoFiniteDiffDynamicsPerformance(MujocoFiniteDiff, Dynamics):
         C = dt * acc_jacobian[:, :self.sim.model.nq]
         D = np.eye(self.sim.model.nv) + dt * acc_jacobian[:, self.sim.model.nq:]
         state_jacobian = np.vstack([
-            np.hstack([
-                A,
-                B,
-            ]),
-            np.hstack([
-                C,
-                D,
-            ])
+            np.hstack([A, B]),
+            np.hstack([C, D]),
         ])
 
         return state_jacobian
 
     def f_u(self, x, u, i):
-        self.set_state(x)
-        self.sim.data.ctrl[:] = u
+        """Partial derivative of dynamics model w.r.t. to u.
+           See base class Dynamics.
+
+           Assumes that the control input u is mapped into raw control ctrl
+           in the simulator by a function that does not depend on x."""
+        self._set_state_action(x, u)
         self._warmstart()
-        self.sim.forward()
 
         # Finite difference over control: skip = VEL
         # This Jacobian is qacc differentiated w.r.t. ctrl.
-        jacobian_acc = finite_diff(self.sim, self.sim.data.ctrl, self.warmstart,
-                               SkipStages.VEL, self._u_eps)
+        jacobian_acc = _finite_diff(self.sim, self.sim.data.ctrl, self.warmstart,
+                                    SkipStages.VEL, self._u_eps)
         # But we want (qpos, qvel) differentiated w.r.t. ctrl.
         # \nabla_{u_t} \ddot{x}_t = acc_jacobian
         # \nabla_{u_t} \dot{x}_t = dt * \nabla_{u_t} \ddot{x}_t
@@ -378,21 +257,17 @@ class MujocoFiniteDiffDynamicsPerformance(MujocoFiniteDiff, Dynamics):
         return state_jacobian
 
     def f_xx(self, x, u, i):
+        """See base class Dynamics."""
         raise NotImplementedError()
 
     def f_ux(self, x, u, i):
+        """See base class Dynamics."""
         raise NotImplementedError()
 
     def f_uu(self, x, u, i):
+        """See base class Dynamics."""
         raise NotImplementedError()
 
-    @property
-    def has_hessians(self):
-        return False
-
-    def get_state(self):
-        return MujocoPerformanceState.from_mjdata(self.sim.data).flatten()
-
-    def set_state(self, x):
-        state = MujocoPerformanceState.from_flattened(x, self.sim)
-        state.set_mjdata(self.sim.data)
+    def _set_state_action(self, x, u):
+        self.sim.data.ctrl[:] = u
+        self.set_state(x)
