98cda7d52c168c90fe22e27b816c5ffbc5021488
Adam Gleave <adam@gleave.me>
2019-01-28 19:58:23 -0800

    MuJoCo control: inverted pendulum cost + demo

--- aprl/envs/mujoco_costs.py
+++ aprl/envs/mujoco_costs.py
@@ -17,8 +17,7 @@ class ReacherCost(BatchAutoDiffCost):
             if terminal:
                 return T.zeros_like(x[..., 0])
 
-            import theano
-            # x: (8, batch_size)
+            # x: (batch_size, 8)
             # x[..., 0:4]: qpos
             # x[..., 4:8]: qvel, time derivatives of qpos, not used in the cost.
             theta = x[..., 0]  # qpos[0]: angle of joint 0
@@ -36,3 +35,32 @@ class ReacherCost(BatchAutoDiffCost):
             return cost
 
         super().__init__(f, state_size=8, action_size=2)
+
+
+class InvertedPendulumCost(BatchAutoDiffCost):
+    '''Differentiable cost for the InvertedPendulum-v2 Gym environment.
+       InvertedPendulum-v2 has a +1 reward while pendulum is upright, and 0
+       otherwise (with the episode terminating). This is problematic to use with
+       iLQG: the cost is not differentiable (indeed, it is discontinuous on the
+       state), and handling episode termination is awkward (effectively the
+       dynamics include transition into a zero-reward absorbing state). Instead,
+       I use a cost function penalizing the square of the: angle from y-axis,
+       velocity and control. (The control penalty seems to be unnecessary to get
+       good performance, but the velocity term is needed.)'''
+    def __init__(self):
+        def f(x, u, i, terminal):
+            if terminal:
+                return T.zeros_like(x[..., 0])
+            # x: (batch_size, 4), concatenation of qpos & qvel
+
+            angle = x[..., 1]  # pendulum rotation
+            ang_cost = angle * angle  # penalize large angles
+            vel = x[..., 2:4]
+            vel_cost = T.sum(vel * vel, axis=-1)  # penalize large velocities
+            ctrl_cost = T.sum(u * u, axis=-1)  # penalize large control
+            # Try and keep the pendulum as upright as possible,
+            # without too rapid movement.
+            cost = ang_cost + vel_cost + ctrl_cost
+            return cost
+
+        super().__init__(f, state_size=4, action_size=1)
