cb5d764beb106011f03808e572e43ae6a19b2cd7
Adam Gleave <adam@gleave.me>
2019-02-02 10:33:52 -0800

    Add costs for DoublePendulum, Hopper and Swimmer

--- aprl/envs/mujoco_costs.py
+++ aprl/envs/mujoco_costs.py
@@ -9,13 +9,16 @@ defined in terms of the raw MuJoCo state (qpos, qvel), not Gym observations.'''
 from theano import tensor as T
 from ilqr.cost import BatchAutoDiffCost
 
+
 class ReacherCost(BatchAutoDiffCost):
     '''Differentiable cost for the Reacher-v2 Gym environment.
        See base class for more details.'''
     def __init__(self):
         def f(x, u, i, terminal):
             if terminal:
-                return T.zeros_like(x[..., 0])
+                ctrl_cost = T.zeros_like(x[..., 0])
+            else:
+                ctrl_cost = T.square(u).sum(axis=-1)
 
             # x: (batch_size, 8)
             # x[..., 0:4]: qpos
@@ -29,8 +32,7 @@ class ReacherCost(BatchAutoDiffCost):
             delta = tip_xpos - target_xpos
 
             state_cost = T.sqrt(T.sum(delta * delta, axis=-1))
-            control_cost = T.sum(u * u, axis=-1)
-            cost = state_cost + control_cost
+            cost = state_cost + ctrl_cost
 
             return cost
 
@@ -50,17 +52,146 @@ class InvertedPendulumCost(BatchAutoDiffCost):
     def __init__(self):
         def f(x, u, i, terminal):
             if terminal:
-                return T.zeros_like(x[..., 0])
-            # x: (batch_size, 4), concatenation of qpos & qvel
+                ctrl_cost = T.zeros_like(x[..., 0])
+            else:
+                ctrl_cost = T.square(u).sum(axis=-1)  # penalize large control
 
+            # x: (batch_size, 4), concatenation of qpos & qvel
             angle = x[..., 1]  # pendulum rotation
             ang_cost = angle * angle  # penalize large angles
             vel = x[..., 2:4]
-            vel_cost = T.sum(vel * vel, axis=-1)  # penalize large velocities
-            ctrl_cost = T.sum(u * u, axis=-1)  # penalize large control
+            vel_cost = T.square(vel).sum(axis=-1)  # penalize large velocities
+
             # Try and keep the pendulum as upright as possible,
             # without too rapid movement.
             cost = ang_cost + vel_cost + ctrl_cost
             return cost
 
         super().__init__(f, state_size=4, action_size=1)
+
+
+class InvertedDoublePendulumCost(BatchAutoDiffCost):
+    '''Differentiable cost for the InvertedDoublePendulum-v2 Gym environment.
+    The cost construction is a little surprisingly quite different from
+    InvertedPendulum-v2. Gym gives an alive bonus of 10, minus a quadratic
+    penalty for the distance of the tip of the pole from the target position
+    (x at the origin, pole fully upright at y=2) and velocity. Termination
+    condition is just if height drops below y=1. In our implementation, we omit
+    the alive bonus and represent termination condition as a quadratic penalty
+    below a height of 1.1. We also introduce a control penalty.'''
+    def __init__(self, ctrl_coef=1e-1):
+        def f(x, u, i, terminal):
+            # Original Gym does not impose a control cost, but does clip it
+            # to [-1, 1]. This non-linear dynamics is hard for iLQG to handle,
+            # so add a quadratic control penalty instead.
+            if terminal:
+                ctrl_cost = T.zeros_like(x[..., 0])
+            else:
+                ctrl_cost = T.square(u).sum(axis=-1)
+
+            # x: (batch_size, 6), concatenation of qpos & qvel
+
+            # Distance cost
+            # The tricky part is finding Cartesian coords of pole tip.
+            base_x = x[..., 0]  # qpos[0]: x axis of the slider
+            hinge1_ang = x[..., 1]  # qpos[1]: angle of the first hinge
+            hinge2_ang = x[..., 2]  # qpos[2]: angle of the second hinge
+            hinge2_cum_ang = hinge1_ang + hinge2_ang
+            # 0 degrees is y=1, x=0; rotates clockwise.
+            hinge1_x, hinge1_y = T.sin(hinge1_ang), T.cos(hinge1_ang)
+            hinge2_x, hinge2_y = T.sin(hinge2_cum_ang), T.cos(hinge2_cum_ang)
+            tip_x = base_x + hinge1_x + hinge2_x
+            tip_y = hinge1_y + hinge2_y
+            dist_cost = 0.01 * T.square(tip_x) + T.square(tip_y - 2)
+
+            # Velocity cost
+            v1 = x[..., 4]  # qvel[1]
+            v2 = x[..., 5]  # qvel[2]
+            vel_cost = 1e-3 * T.square(v1) + 5e-3 * T.square(v2)
+
+            #TODO: termination penalty? (shouldn't change optimal policy?)
+            termination_cost = T.square(T.max([T.zeros_like(tip_y), 1.1 - tip_y], axis=0))
+
+            cost = dist_cost + vel_cost + ctrl_coef * ctrl_cost
+            return cost
+
+        super().__init__(f, state_size=6, action_size=1)
+
+
+class HopperCost(BatchAutoDiffCost):
+    '''Differentiable cost for the Hopper-v2 Gym environment.
+    I follow Gym in rewarding forward motion, and placing a quadratic penalty
+    on control cost. Gym has a complicated termination condition: since there
+    is a living reward of +1, this loosely corresponds to a discontinuous
+    penalty for violating these conditions. I approximate this by penalizing
+    a low height, extreme angle, or extremely large state vectors.'''
+    def __init__(self):
+        def f(x, u, i, terminal):
+
+            # x: (batch_size, 12), concatenation of qpos & qvel
+            # Gym reward is 1 + forward_movement - control penalty
+            #
+            # We drop the living reward; since we plan over a fixed horizon,
+            # with no termination, constants do not matter.
+            #
+            # Gym computes the forward movement by taking a difference before
+            # and after MuJoCo's step. A MuJoCo step involves numerical
+            # integration on qvel. We can't step MuJoCo here, so use qvel
+            # directly; this should give the same first-order result.
+            forward_movement = x[..., 6]  # qvel[0]
+
+            # We calculate the quadratic control penalty as in Gym.
+            if terminal:
+                control_penalty = 0
+            else:
+                control_penalty = 1e-3 * T.square(u).sum(axis=-1)
+
+            # The Gym done condition is:
+            # Any state nan/infinite; absolute value of any x[..., 2:] >= 100;
+            # height <= .7; absolute value of angle x[..., 2] >= .2.
+            # We can't handle termination conditions, but treat violation of
+            # these constraints as penalty since living reward is positive.
+            # I don't do anything to try and handle nan/infinite case; infinity
+            # will be discouraged by the penalty, and I've not run into any
+            # issues with numerical stability.
+            height = x[..., 1]  # qpos[1]
+            abs_ang = abs(x[..., 2])  # qpos[2]
+            def penalty_geq(x, target):
+                '''Quadratic penalty if x > target; zero cost if x <= target.'''
+                return T.square(T.max([T.zeros_like(x), x - target * 0.9], axis=0))
+            angle_penalty = 100 * penalty_geq(abs_ang, 0.2)
+            height_penalty = 100 * penalty_geq(-height, -0.7)
+            state_penalty = 1e-4 * T.sum(penalty_geq(abs(x[..., 2:]), 100), axis=-1)
+            termination_penalty = angle_penalty + height_penalty + state_penalty
+
+            cost = -forward_movement + control_penalty + termination_penalty
+            return cost
+
+        super().__init__(f, state_size=12, action_size=3)
+
+
+class SwimmerCost(BatchAutoDiffCost):
+    '''Differentiable cost for the Swimmer-v2 Gym environment. Cost function is
+    as in Gym, except using velocity variable directly rather than taking
+    finite difference.'''
+    def __init__(self):
+        def f(x, u, i, terminal):
+            # x: (batch_size, 10), concatenation of qpos & qvel
+            # Gym reward is forward_movement - control_cost.
+            #
+            # Gym computes the forward movement by taking a difference before
+            # and after MuJoCo's step. A MuJoCo step involves numerical
+            # integration on qvel. We can't step MuJoCo here, so use qvel
+            # directly; this should give the same first-order result.
+            forward_movement = x[..., 5]  # qvel[0]
+
+            # We calculate the quadratic control penalty as in Gym.
+            if terminal:
+                control_penalty = 0
+            else:
+                control_penalty = 1e-4 * T.square(u).sum(axis=-1)
+
+            cost = -forward_movement + control_penalty
+            return cost
+
+        super().__init__(f, state_size=10, action_size=2)
