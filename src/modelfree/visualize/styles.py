93fee28427dee32398039dda20190c6ec9b48c65
Adam Gleave <adam@gleave.me>
2019-04-27 20:41:19 -0700

    Visualization: add generator for training curves, convert everything to Sacred experiments
    Squashed commit of the following:
    
    commit 074a5921329875cc52302c55627b005cbfbf4668
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 18:44:07 2019 -0700
    
        Include ID number in Zoo baseline
    
    commit 084801eb27f4990318490ee88d8784bda13c0e9a
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 16:21:45 2019 -0700
    
        Add white grid background
    
    commit 58c4f0a14412a379977e90b97f66169d34e319c1
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 16:12:49 2019 -0700
    
        Bugfix: remove spurious legend early enough to not cause off-by-one in color map
    
    commit 6e29634ae08ab602b5f1a65393e28939991401b8
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 15:50:23 2019 -0700
    
        Styles: allow room for caption
    
    commit 8e7cbf88f1462276879b55cedfbaae569e3f4ebe
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 15:36:14 2019 -0700
    
        Make margins and legends auto adjust to figure size
    
    commit 01c4466106894453d607df5179d13485713575b1
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 15:28:02 2019 -0700
    
        Monolithic supplementary, labels per seed
    
    commit d6deeac0fe40037ebc245306cfb2eb40d3967631
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 14:24:59 2019 -0700
    
        Prettify legend
    
    commit 22526ed4ddcd3fefcf0e8c0e06aec97a0ee15141
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 14:19:57 2019 -0700
    
        Add titles to monolithic plot
    
    commit dc0580d5079a4c5fb8f5dc741cfdc8dd217f9776
    Author: Adam Gleave <adam@gleave.me>
    Date:   Sat Apr 27 13:33:09 2019 -0700
    
        Monolithic plot
    
    commit 85bd81b1404581e3481fc0a6802833be9e50b34f
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 21:27:38 2019 -0700
    
        Formatting tweaks
    
    commit 4ec91c79b7115dcc8089e9777b253506c30a99d5
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 21:09:35 2019 -0700
    
        Resize to make axis labels visible
    
    commit 24563b0dcb62f3ddae111790fb3a28cb2cb57113
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 20:51:07 2019 -0700
    
        Cleanup visualize.training
    
    commit 0d6451a1737e75c11fe3977827e46a11cf9c92fb
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 20:41:10 2019 -0700
    
        Ugly but working multiprocessing for visualize.training
    
    commit 5c660c2dcca02c3ea70ed3fd67b2913ae5a8b009
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 18:50:44 2019 -0700
    
        Add horizontal lines for baselines
    
    commit d7f1b1c45eb6549424f34cc86df2addc7390564c
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 18:05:55 2019 -0700
    
        Make plotting style configurable
    
    commit 1ede6968b5e9baf821226312d6aeae0448805b6f
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 17:52:25 2019 -0700
    
        Prototype plotting TensorBoard training curves
    
    commit b981b3b087e44524c898895c3ffe26bc3e05952c
    Author: Adam Gleave <adam@gleave.me>
    Date:   Fri Apr 26 15:02:01 2019 -0700
    
        Convert visualize.scores to use Sacred
    
    commit b1712419392e0111045a7c47dbd3e5b66d0abaab
    Author: Adam Gleave <adam@gleave.me>
    Date:   Thu Apr 25 21:54:10 2019 -0700
    
        Customize score heatmaps for publication

--- src/modelfree/visualize/styles.py
+++ src/modelfree/visualize/styles.py
@@ -1,10 +1,23 @@
 STYLES = {
     'paper': {
+        'figure.figsize': (5.5, 7.5),
         'font.serif': 'Times New Roman',
         'font.family': 'serif',
-        'font.size': 10,
-        'legend.fontsize': 10,
-        'figure.figsize': (5.5, 4.125)
+        'font.size': 9,
+        'legend.fontsize': 9,
+        'axes.titlesize': 9,
+        'axes.labelsize': 9,
+        'xtick.labelsize': 9,
+        'ytick.labelsize': 9,
+    },
+    'monolithic': {
+        'figure.figsize': (5.5, 2.0625),
+    },
+    'twocol': {
+        'figure.figsize': (2.75, 2.0625),
+    },
+    'threecol': {
+        'figure.figsize': (1.83, 1.7),
     },
     'a4': {
         'figure.figsize': (8.27, 11.69),
