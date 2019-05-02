8dce2df9f0a5f3fad088ef16210715acb892e62d
Adam Gleave <adam@gleave.me>
2019-05-02 15:52:52 -0700

    Overlay text with agent types

--- src/modelfree/visualize/annotated_gym_compete.py
+++ src/modelfree/visualize/annotated_gym_compete.py
@@ -11,17 +11,9 @@ from modelfree.envs import VICTIM_INDEX
 from modelfree.envs.gym_compete import env_name_to_canonical, game_outcome
 
 VICTIM_OPPONENT_COLORS = {
-    'Victim': (128, 128, 128, 255),
-    'Opponent': (255, 0, 0, 255),
+    'Victim': (55, 126, 184, 255),
+    'Opponent': (228, 26, 28, 255),
     'Ties': (0, 0, 0, 255),
-}
-
-
-POLICY_TYPE_COLORS = {
-    'zoo': (128, 128, 128, 255),  # grey
-    'ppo2': (255, 0, 0, 255),  # red
-    'zero': (0, 0, 0, 255),  # black
-    'random': (0, 0, 255, 255),  # blue
 }
 
 
@@ -30,19 +22,8 @@ def body_color(is_victim, agent_type, agent_path):
     return VICTIM_OPPONENT_COLORS[key]
 
 
-def head_color(is_victim, agent_type, agent_path):
-    return POLICY_TYPE_COLORS[agent_type]
-
-
 GEOM_MAPPINGS = {
     '*': body_color,
-    # Ant
-    'torso_geom': head_color,
-    # Humanoid
-    'torso1': head_color,
-    'uwaist': head_color,
-    'lwaist': head_color,
-    'head': head_color,
 }
 
 
@@ -68,7 +49,7 @@ def set_geom_colors(model, patterns):
 
 CAMERA_CONFIG = {
     # From behind
-    'KickAndDefend-v0': {'azimuth': 0, 'distance': 10, 'elevation': -23},
+    'KickAndDefend-v0': {'azimuth': 0, 'distance': 10, 'elevation': -19},
     # From side, slightly behind (runner always goes forward, never back)
     'YouShallNotPassHumans-v0': {'azimuth': 110, 'distance': 9, 'elevation': -21},
     # From side, close up
@@ -77,26 +58,31 @@ CAMERA_CONFIG = {
 }
 
 
+PRETTY_POLICY_TYPES = {
+    'ppo2': 'Adversary (Adv)',
+    'zoo': 'Normal (Zoo)',
+    'zero': 'Lifeless (Zero)',
+    'rand': 'Random (Rand)',
+}
+
+
 class AnnotatedGymCompete(gym.Wrapper):
     def __init__(self, env, env_name, agent_a_type, agent_a_path, agent_b_type, agent_b_path,
-                 resolution, font, font_size, spacing=0.02):
+                 resolution, font, font_size, ypos=0.0, spacing=0.05):
         super(AnnotatedGymCompete, self).__init__(env)
 
         # Set agent colors
         self.env_name = env_name
         self.victim_index = VICTIM_INDEX[env_name]
-        agent_mapping = {
-            'agent0': (0 == self.victim_index, agent_a_type, agent_a_path),
-            'agent1': (1 == self.victim_index, agent_b_type, agent_b_path)
-        }
-        color_patterns = {f'{agent_key}/{geom_key}': geom_fn(*agent_val)
-                          for geom_key, geom_fn in GEOM_MAPPINGS.items()
-                          for agent_key, agent_val in agent_mapping.items()}
-        set_geom_colors(self.env.unwrapped.env_scene.model, color_patterns)
+        self.agent_a_type = agent_a_type
+        self.agent_a_path = agent_a_path
+        self.agent_b_type = agent_b_type
+        self.agent_b_path = agent_b_path
 
         # Text overlay
         self.font = ImageFont.truetype(f'{font}.ttf', font_size)
         self.font_bold = ImageFont.truetype(f'{font}bi.ttf', font_size)
+        self.ypos = ypos
         self.spacing = spacing
 
         # Internal state
@@ -115,6 +101,17 @@ class AnnotatedGymCompete(gym.Wrapper):
         self.camera_setup()
 
     def camera_setup(self):
+        # Color mapping
+        agent_mapping = {
+            'agent0': (0 == self.victim_index, self.agent_a_type, self.agent_a_path),
+            'agent1': (1 == self.victim_index, self.agent_b_type, self.agent_b_path),
+        }
+        color_patterns = {f'{agent_key}/{geom_key}': geom_fn(*agent_val)
+                          for geom_key, geom_fn in GEOM_MAPPINGS.items()
+                          for agent_key, agent_val in agent_mapping.items()}
+        set_geom_colors(self.env.unwrapped.env_scene.model, color_patterns)
+
+        # Camera setup
         canonical_env_name = env_name_to_canonical(self.env_name)
         camera_cfg = CAMERA_CONFIG[canonical_env_name]
         viewer = self.env.unwrapped.env_scene.viewer
@@ -150,27 +147,40 @@ class AnnotatedGymCompete(gym.Wrapper):
             draw = ImageDraw.Draw(img)
 
             width, height = img.size
-            ypos = height * 0.85
+            ypos = height * self.ypos
             texts = collections.OrderedDict([
                 (f'win{1 - self.victim_index}', 'Opponent'),
-                (f'win{self.victim_index}', 'Victim'),
                 ('ties', 'Ties'),
+                (f'win{self.victim_index}', 'Victim'),
             ])
 
             to_draw = []
             for k, label in texts.items():
-                msg = f'{label} = {self.result[k]}'
+                pol_type = None
+                header = ""
+                if label == 'Opponent':
+                    pol_type = self.agent_b_type if self.victim_index == 0 else self.agent_a_type
+                elif label == 'Victim':
+                    pol_type = self.agent_a_type if self.victim_index == 0 else self.agent_b_type
+                if pol_type is not None:
+                    header = PRETTY_POLICY_TYPES[pol_type]
+
+                scores = f"{label} = {self.result[k]}"
+
                 color = VICTIM_OPPONENT_COLORS[label]
                 font = self.font_bold if k == self.last_won else self.font
-                to_draw.append((msg, font, color))
+                to_draw.append(([scores, header], font, color))
 
-            lengths = [font.getsize(msg)[0] + self.spacing * width
-                       for (msg, font, color) in to_draw]
+            lengths = []
+            for msgs, font, color in to_draw:
+                length = max((font.getsize(msg)[0] for msg in msgs))
+                lengths.append(length + self.spacing * width)
             total_length = sum(lengths)
             xpos = (width - total_length) / 2
 
-            for (msg, font, color), length in zip(to_draw, lengths):
-                draw.text((xpos, ypos), msg, font=font, fill=color)
+            for (msgs, font, color), length in zip(to_draw, lengths):
+                text = "\n".join(msgs)
+                draw.multiline_text((xpos, ypos), text, align="center", font=font, fill=color)
                 xpos += length
 
             res = np.array(img)
