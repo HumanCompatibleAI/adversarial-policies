a270cb3823add6693b27b7ccefa0465fd06635ee
Adam Gleave <adam@gleave.me>
2019-05-03 19:14:44 -0700

    Parallelize fit_tsne.py

--- src/modelfree/interpretation/fit_tsne.py
+++ src/modelfree/interpretation/fit_tsne.py
@@ -7,6 +7,7 @@ import tempfile
 
 import numpy as np
 import pandas as pd
+import ray
 import sacred
 from sacred.observers import FileStorageObserver
 from sklearn.manifold import TSNE
@@ -17,6 +18,7 @@ logger = logging.getLogger('modelfree.interpretation.fit_tsne')
 
 @fit_tsne_ex.config
 def base_config():
+    ray_server = None  # by default will launch a server
     activation_dir = None
     output_root = None
     data_type = 'ff_policy'
@@ -35,7 +37,6 @@ def debug_config():
     del _
 
 
-@fit_tsne_ex.capture
 def _load_and_reshape_single_file(np_path, opponent_type, data_type):
     traj_data = np.load(np_path, allow_pickle=True)
     episode_list = traj_data[data_type].tolist()
@@ -59,12 +60,15 @@ def _load_and_reshape_single_file(np_path, opponent_type, data_type):
     return concatenated_data, metadata_df
 
 
-def fit_tsne_helper(activation_paths, output_dir, num_components, num_observations, perplexity):
+@ray.remote
+def fit_tsne_helper(activation_paths, output_dir, num_components, num_observations,
+                    perplexity, data_type):
     all_file_data = []
     all_metadata = []
+    artifacts = []
     for opponent_type, path in activation_paths.items():
         logger.debug(f"Loaded data for {opponent_type} from {path}")
-        file_data, metadata = _load_and_reshape_single_file(path, opponent_type)
+        file_data, metadata = _load_and_reshape_single_file(path, opponent_type, data_type)
         all_file_data.append(file_data)
         all_metadata.append(metadata)
 
@@ -79,29 +83,33 @@ def fit_tsne_helper(activation_paths, output_dir, num_components, num_observatio
     # Save metadata
     metadata_path = os.path.join(output_dir, 'metadata.csv')
     merged_metadata[0:num_observations].to_csv(metadata_path)
-    fit_tsne_ex.add_artifact(metadata_path)
+    artifacts.append(metadata_path)
 
     # Fit t-SNE
     tsne_obj = TSNE(n_components=num_components, verbose=1, perplexity=perplexity)
     logger.info(f"Starting T-SNE fitting, saving to {output_dir}")
     tsne_ids = tsne_obj.fit_transform(sub_data)
-    logger.info(f"Completed T-SNE fitting, saved to ")
+    logger.info(f"Completed T-SNE fitting, saved to {output_dir}")
 
     # Save weights
     tsne_weights_path = os.path.join(output_dir, 'tsne_weights.pkl')
     with open(tsne_weights_path, "wb") as fp:
         pickle.dump(tsne_obj, fp)
-    fit_tsne_ex.add_artifact(tsne_weights_path)
+    artifacts.append(tsne_weights_path)
 
     # Save cluster IDs
     cluster_ids_path = os.path.join(output_dir, 'cluster_ids.npy')
     np.save(cluster_ids_path, tsne_ids)
-    fit_tsne_ex.add_artifact(cluster_ids_path)
+    artifacts.append(cluster_ids_path)
+
+    return artifacts
 
 
 @fit_tsne_ex.main
-def fit_tsne(activation_dir, output_root,
-             num_components, num_observations, perplexity):
+def fit_tsne(ray_server, activation_dir, output_root,
+             num_components, num_observations, perplexity, data_type):
+    ray.init(redis_address=ray_server)
+
     # Find activation paths for each environment & victim-path tuple
     stem_pattern = re.compile(r'(.*)_opponent_.*\.npz')
     opponent_pattern = re.compile(r'.*_opponent_([^\s]+)_[^\s]+\.npz')
@@ -126,15 +134,24 @@ def fit_tsne(activation_dir, output_root,
         output_root = tmp_dir.name
 
     # Fit t-SNE and save model weights
-    # TODO: parallelize?
+    artifacts = {}
     for stem, paths in activation_paths.items():
         output_dir = osp.join(output_root, stem)
         os.makedirs(output_dir)
-        fit_tsne_helper(paths, output_dir, num_components, num_observations, perplexity)
+        future = fit_tsne_helper.remote(paths, output_dir, num_components,
+                                        num_observations, perplexity, data_type)
+        artifacts[stem] = future
+
+    for stem, paths_future in artifacts.items():
+        paths = ray.get(paths_future)
+        for path in paths:
+            fit_tsne_ex.add_artifact(path)
 
     # Clean up temporary directory (if needed)
     if tmp_dir is not None:
         tmp_dir.clean()
+
+    ray.shutdown()
 
 
 def main():
