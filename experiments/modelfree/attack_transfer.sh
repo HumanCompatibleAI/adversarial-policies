7d212727dba96706f79caf065c0f6b0597fd484d
Adam Gleave <adam@gleave.me>
2019-05-05 10:34:31 -0700

    Optionally submit jobs in parallel

--- experiments/modelfree/attack_transfer.sh
+++ experiments/modelfree/attack_transfer.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+function wait_proc {
+    if [[ -f ~/ray_bootstrap_config.yaml ]]; then
+        # Running on a Ray cluster. We want to submit all the jobs in parallel.
+        true  # no-op
+    else
+        # Running locally. Each job will start a Ray cluster. Submit sequentially.
+        wait
+    fi
+}
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUT_ROOT=data/aws/score_agents
 TIMESTAMP=`date --iso-8601=seconds`
@@ -19,10 +29,18 @@ ADVERSARY_PATHS=${OUT_ROOT}/normal/${TIMESTAMP}/best_adversaries.json
 python ${DIR}/highest_win_rate.py ${ADVERSARY_PATHS} --logdir $*
 
 export ADVERSARY_PATHS=${ADVERSARY_PATHS}
-${MULTI_SCORE_CMD} save_path=${OUT_ROOT}/normal/${TIMESTAMP}/adversary_transfer.json
+
+${MULTI_SCORE_CMD} save_path=${OUT_ROOT}/normal/${TIMESTAMP}/adversary_transfer.json&
+wait_proc
+
 ${MULTI_SCORE_CMD} mask_observations_of_victim \
-                   save_path=${OUT_ROOT}/victim_masked_init/${TIMESTAMP}/adversary_transfer.json
+    save_path=${OUT_ROOT}/victim_masked_init/${TIMESTAMP}/adversary_transfer.json&
+wait_proc
+
 ${MULTI_SCORE_CMD} mask_observations_of_victim score.mask_agent_kwargs.masking_type=zeros \
-                   save_path=${OUT_ROOT}/victim_masked_zero/${TIMESTAMP}/adversary_transfer.json
+    save_path=${OUT_ROOT}/victim_masked_zero/${TIMESTAMP}/adversary_transfer.json&
+wait_proc
+
 ${MULTI_SCORE_CMD} mask_observations_of_adversary \
-                   save_path=${OUT_ROOT}/adversary_masked_init/${TIMESTAMP}/adversary_transfer.json
+    save_path=${OUT_ROOT}/adversary_masked_init/${TIMESTAMP}/adversary_transfer.json&
+wait_proc
--- experiments/modelfree/baselines.sh
+++ experiments/modelfree/baselines.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
+function wait_proc {
+    if [[ -f ~/ray_bootstrap_config.yaml ]]; then
+        # Running on a Ray cluster. We want to submit all the jobs in parallel.
+        true  # no-op
+    else
+        # Running locally. Each job will start a Ray cluster. Submit sequentially.
+        wait
+    fi
+}
+
 CMD="python -m modelfree.multi.score with high_accuracy"
 OUT_DIR=data/aws/score_agents
 
 mkdir -p ${OUT_DIR}
 for kind in zoo fixed; do
     mkdir -p ${OUT_DIR}/normal
-    ${CMD} ${kind}_baseline save_path=${OUT_DIR}/normal/${kind}_baseline.json
+    ${CMD} ${kind}_baseline save_path=${OUT_DIR}/normal/${kind}_baseline.json&
+    wait_proc
+
     mkdir -p ${OUT_DIR}/victim_masked_init
     ${CMD} ${kind}_baseline mask_observations_of_victim \
-           save_path=${OUT_DIR}/victim_masked_init/${kind}_baseline.json
+           save_path=${OUT_DIR}/victim_masked_init/${kind}_baseline.json&
+    wait_proc
+
     mkdir -p ${OUT_DIR}/victim_masked_zero
     ${CMD} ${kind}_baseline mask_observations_of_victim \
            score.mask_agent_kwargs.masking_type=zeros \
-           save_path=${OUT_DIR}/victim_masked_zero/${kind}_baseline.json
+           save_path=${OUT_DIR}/victim_masked_zero/${kind}_baseline.json&
+    wait_proc
 done
