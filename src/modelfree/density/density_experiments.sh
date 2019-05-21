e93cea418e0266922c7bd5ad57fce30eea0a87b8
Adam Gleave <adam@gleave.me>
2019-05-21 15:05:00 +0100

    Density experiments bugfix: correct path

--- src/modelfree/density/density_experiments.sh
+++ src/modelfree/density/density_experiments.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ADV_PATH="data/aws/score_agents/2019-05-05T18:12:24+00:00/best_adversaries.json"
+ADV_PATH="data/aws/score_agents/normal/2019-05-05T18:12:24+00:00/best_adversaries.json"
 
 python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
 python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
