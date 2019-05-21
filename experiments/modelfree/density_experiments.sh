7c875d4ee47cd61517aa71f523132f0eed5ae855
Adam Gleave <adam@gleave.me>
2019-05-21 16:46:08 +0100

    Typo

--- experiments/modelfree/density_experiments.sh
+++ experiments/modelfree/density_experiments.sh
@@ -8,7 +8,7 @@ ADV_PATH="data/aws/score_agents/normal/2019-05-05T18:12:24+00:00/best_adversarie
 
 for components in 1 2 3 5 10; do
     for cov_type in full diag spherical; do
-        python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=${componets} \
+        python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=${components} \
                                                       fit_density_model.model_kwargs.covariance_type=${cov_type} \
                                                       generate_activations.adversary_path=${ADV_PATH}&
         wait_proc
