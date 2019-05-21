9f834474b84cb33666fa1d50809ab8353679ed07
Adam Gleave <adam@gleave.me>
2019-05-21 12:13:50 +0100

    Increase parallelism in density_experiments

--- src/modelfree/density/density_experiments.sh
+++ src/modelfree/density/density_experiments.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 ADV_PATH="data/aws/score_agents/2019-05-05T18:12:24+00:00/best_adversaries.json"
 
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=full generate_activations.adversary_path=${ADV_PATH}&
+wait
 
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=diag generate_activations.adversary_path=${ADV_PATH}&
+wait
 
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}
-python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}&
+python -m modelfree.density.pipeline with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=spherical generate_activations.adversary_path=${ADV_PATH}&
+wait
