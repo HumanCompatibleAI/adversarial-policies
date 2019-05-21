5105aeeb7d02bc10efdb78b17be416550fd7bb5d
Cody Wild <cody@airbears2-10-142-32-181.airbears2.1918.berkeley.edu>
2019-05-20 17:47:03 -0700

    local updates to get high accuracy density hyperparameter search

--- src/modelfree/density/density_experiments.sh
+++ src/modelfree/density/density_experiments.sh
@@ -4,16 +4,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ADV_PATH="data/aws/score_agents/2019-05-05T18:12:24+00:00/best_adversaries.json"
 
 python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=full tsne_activations.adversary_path=${ADV_PATH}
 
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=diag
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=diag
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=diag
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=diag
-#
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=spherical
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=spherical
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=spherical
-#python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=spherical
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=diag tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=diag tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=diag tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=diag tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=diag tsne_activations.adversary_path=${ADV_PATH}
+
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=1 fit_density_model.model_kwargs.covariance_type=spherical tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=2 fit_density_model.model_kwargs.covariance_type=spherical tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=3 fit_density_model.model_kwargs.covariance_type=spherical tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=5 fit_density_model.model_kwargs.covariance_type=spherical tsne_activations.adversary_path=${ADV_PATH}
+python ${DIR}/pipeline.py with gmm fit_density_model.model_kwargs.n_components=10 fit_density_model.model_kwargs.covariance_type=spherical tsne_activations.adversary_path=${ADV_PATH}
