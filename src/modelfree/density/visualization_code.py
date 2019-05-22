28f5df4115bfabbc12c21ee3df82f94fc213f270
Cody Wild <cody@airbears2-10-142-32-134.airbears2.1918.berkeley.edu>
2019-05-21 18:23:59 -0700

    added hyperparameter heatmap code

--- src/modelfree/density/visualization_code.py
+++ src/modelfree/density/visualization_code.py
@@ -1,9 +1,13 @@
+from glob import glob
+import json
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+DENSITY_DIR = "density"
 
 
 def opponent_convert(x):
@@ -12,19 +16,32 @@ def opponent_convert(x):
     else:
         return x
 
-# This was run on top of the density directories with timestamp, and fitted/activations inside
 
-
-def get_train_test_merged_df(env, victim_id, exp_id="20190517_114008"):
+def get_full_directory(env, victim_id, n_components, covariance):
+    hp_dir = f"{DENSITY_DIR}/gmm_{n_components}_components_{covariance}"
+    exp_dir = glob(hp_dir + "/*")[0]
     env_dir = f"{env}-v0_victim_zoo_{victim_id}"
-    train_df = pd.read_csv(os.path.join(exp_id, 'fitted', env_dir, "train_metadata.csv"))
+    full_env_dir = os.path.join(exp_dir, 'fitted', env_dir)
+    return full_env_dir
+
+
+def get_train_test_merged_df(env, victim_id, n_components, covariance):
+    full_env_dir = get_full_directory(env, victim_id, n_components, covariance)
+    train_df = pd.read_csv(os.path.join(full_env_dir, "train_metadata.csv"))
     train_df['is_train'] = True
-    test_df = pd.read_csv(os.path.join(exp_id, 'fitted', env_dir, "test_metadata.csv"))
+    test_df = pd.read_csv(os.path.join(full_env_dir, "test_metadata.csv"))
     test_df['is_train'] = False
     merged = pd.concat([train_df, test_df])
     merged['broad_opponent'] = merged['opponent_id'].apply(lambda x: opponent_convert(x))
     merged['raw_proba'] = np.exp(merged['log_proba'])
     return merged
+
+
+def get_metrics_dict(env, victim_id, n_components, covariance):
+    full_env_dir = get_full_directory(env, victim_id, n_components, covariance)
+    with open(os.path.join(full_env_dir, 'metrics.json'), 'r') as fp:
+        metric_dict = json.load(fp)
+    return metric_dict
 
 
 def comparative_densities(group_var, env_name, victim, shade=False, cutoff_point=None):
@@ -49,7 +66,49 @@ def comparative_densities(group_var, env_name, victim, shade=False, cutoff_point
     plt.show()
 
 
+def heatmap_plot(env_name, metric, victim=1, savefile=None, error_val=-1):
+    n_component_grid = [1, 2, 3, 5, 10]
+    covariance_grid = ['diag', 'spherical', 'full']
+    metric_grid = np.zeros(shape=(5, 3))
+    if isinstance(metric, str):
+        metric_name = metric
+    else:
+        metric_name = metric.__name__
+
+    for i, n_components in enumerate(n_component_grid):
+        for j, covariance in enumerate(covariance_grid):
+            try:
+                metrics = get_metrics_dict(env_name, victim, n_components, covariance)
+                if isinstance(metric, str):
+                    metric_grid[i][j] = metrics[metric]
+                else:
+                    metric_grid[i][j] = metric(metrics)
+            except FileNotFoundError:
+                print(
+                    f"Hit exception on {env_name}, {n_components} components {covariance},"
+                    f" filling in {error_val}")
+                metric_grid[i][j] = error_val
+
+    ll_df = pd.DataFrame(metric_grid, index=n_component_grid, columns=covariance_grid)
+    plt.figure(figsize=(10, 7))
+    sns.heatmap(ll_df, annot=True)
+    plt.title(f"HP Search on {env_name}, Victim {victim}: {metric_name}")
+    if savefile is not None:
+        plt.savefig(savefile)
+
+
 if __name__ == "__main__":
-    # Example plot
+    # Example Density plot
     comparative_densities(group_var='opponent_id', env_name='KickAndDefend', victim='1',
                           cutoff_point=-1000)
+
+    # Make heatmaps
+    def train_bic_in_millions(x):
+        return x['train_bic'] / 1000000
+    output_dir = "density_plots"
+
+    for env in ['KickAndDefend', 'SumoAnts', 'SumoHumans', 'YouShallNotPass']:
+        heatmap_plot(env_name=env, metric=train_bic_in_millions,
+                     savefile=f"{output_dir}/{env}_train_bic.png")
+        heatmap_plot(env_name=env, metric='validation_log_likelihood',
+                     savefile=f"{output_dir}/{env}_validation_log_likelihood.png")
