cf8b1826d68e8a48a76c9d8088246b363b2ffafe
Cody Wild <cody@airbears2-10-142-32-134.airbears2.1918.berkeley.edu>
2019-05-21 18:20:08 -0700

    small remote changes

--- experiments/modelfree/noisy_actions_and_obs.sh
+++ experiments/modelfree/noisy_actions_and_obs.sh
@@ -11,8 +11,8 @@ function wait_proc {
 }
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#OUT_ROOT=/home/ubuntu/data
-OUT_ROOT=data/aws/score_agents
+OUT_ROOT=/home/ubuntu/data/aws/score_agents
+#OUT_ROOT=data/aws/score_agents
 TIMESTAMP=`date --iso-8601=seconds`
 
 MULTI_SCORE_CMD="python -m modelfree.multi.score with medium_accuracy "
@@ -29,7 +29,7 @@ for dir in noisy_adversary_actions noisy_victim_actions noisy_victim_obs; do
 done
 # Rerun highest_win_rate and store the results in ADVERSARY_PATHS, which we
 # export as an environment variable which multi score will use
-ADVERSARY_PATHS=${OUT_ROOT}/2019-05-05T18:12:24+00:00/best_adversaries.json
+ADVERSARY_PATHS=${OUT_ROOT}/normal/2019-05-05T18:12:24+00:00/best_adversaries.json
 
 export ADVERSARY_PATHS=${ADVERSARY_PATHS}
 
