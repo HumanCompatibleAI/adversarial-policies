99700aab22f99f8353dc74b0ddaf8e5861ff34a5
Neel Kant <kantneel@berkeley.edu>
2019-06-06 03:57:39 -0700

    Add lookback reward shaping (#10)
    * most of LookbackRewardVecWrapper complete
    
    * plugged LookbackRewardVecWrapper into train
    
    * created OldMujocoResettableWrapper and modified MujocoState and MultiMonitor
    
    * integrate lookback_args and OldMujocoResettableWrapper
    
    * LookbackRewardVecWrapper functional but not shaping reward yet
    
    * refactored and also added logic for storing reward and info dicts
    
    * changed 'past' to 'base'. All logic completed, now testing
    
    * bugfix: base_reward assignment vs incrementing
    
    * compatibility with adversarial noise ball
    
    * deterministic policies, truncated obs, debug statements
    
    * lookback operational for rewards, needs to be cleaned up
    
    * refactored to use LookbackDict
    
    * refactored curry_obs and also TransparentLSTMPolicy to fit new LSTMPolicy semantics
    
    * high precision with MlpPolicy rewards. some extraneous code for dealing with transparent stable baselines
    
    * removed extraneous transparent policy handling
    
    * update radius in envs
    
    * all good except for sudden contacts
    
    * cleaned up lookback reward loop
    
    * created lookback_params in order to load arbitrary lookback baseline policies
    
    * bugfix for load_policy finetuning
    
    * WIP mujoco debugging
    
    * s/x_dict/sim_data/
    
    * created DebugVenv which wraps innermost environments
    
    * updated requirements
    
    * wip: trying to debug why buffer doesn't copy over
    
    * lookback works correctly and also runs at reasonable speed
    
    * fixed indexing issue when doing lookback with multiple envs
    
    * changed get_full_state since C pointers are not pickleable
    
    * added lookback_mul param and also removed extraneous logic from lookback step_wait
    
    * made DebugVenv and debug files optional
    
    * s/lb_dict/lb_tuple
    
    * refactored but also broken
    
    * moved responsibility of transparency to VecEnvWrappers
    
    * cleaned up _reset_state_data, added docstrings
    
    * fixed state assignment bug and renamed some vars
    
    * built logic for setting all deterministic=True
    
    * cleaned _reset_state_data, added helper methods and docstrings
    
    * changed adv_noise_params defaults and added lookback test case
    
    * linting and code cleanup
    
    * moved lookback into modelfree.common and also moved lookback mujoco into aprl.common
    
    * reverted MujocoState to just qpos, qvel. Added mujoco docstrings
    
    * removed debug print statement
    
    * refactored and cleaned lookback
    
    * updated requirements-modelfree.txt for new gym_compete
    
    * changed lookback_params, debugging, added helper functions
    
    * changed requirements.txt to have VecEnvWrapper.__getattr__ bugfix
    
    * added modelfree.training.victim_envs
    
    * cleaned code for lookback and pre- and post-wrapping
    
    * updated requirements.txt to better VecEnvWrapper.__getattr__ bug fix
    
    * Code cleanup
    
    * Update requirements.txt to include Stable Baselines bugfix
    
    * Move OldMujocoResettableWrapper to lookback (only place it is used)
    
    * Remove dead code
    
    * Bugfix from merge

--- src/aprl/agents/__init__.py
+++ src/aprl/agents/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa: F401
 
-from aprl.agents.monte_carlo import (MonteCarloParallel, MonteCarloSingle, MujocoResettableWrapper,
-                                     ResettableEnv)
+from aprl.agents.monte_carlo import MonteCarloParallel, MonteCarloSingle, MujocoResettableWrapper
 from aprl.agents.mujoco_lqr import (MujocoFiniteDiffCost, MujocoFiniteDiffDynamicsBasic,
                                     MujocoFiniteDiffDynamicsPerformance)
 from aprl.agents.self_play import SelfPlay
