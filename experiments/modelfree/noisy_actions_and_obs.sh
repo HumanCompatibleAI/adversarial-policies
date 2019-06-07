9e459b70f339a7f1a37d714d86eeeeb6f3f087d8
Cody Wild <cody@airbears2-10-142-34-57.airbears2.1918.berkeley.edu>
2019-06-07 15:29:31 -0700

    factor out wait_prof into common.sh

--- experiments/modelfree/noisy_actions_and_obs.sh
+++ experiments/modelfree/noisy_actions_and_obs.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
-function wait_proc {
-    if [[ -f ~/ray_bootstrap_config.yaml ]]; then
-        # Running on a Ray cluster. We want to submit all the jobs in parallel.
-        sleep 5  # stagger jobs a bit
-    else
-        # Running locally. Each job will start a Ray cluster. Submit sequentially.
-        wait
-    fi
-}
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. ${DIR}/common.sh
 OUT_ROOT=data/aws/score_agents
 TIMESTAMP=`date --iso-8601=seconds`
 
