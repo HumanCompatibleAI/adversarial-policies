ccbd8bbc2377f24f7ec9517a3717ab757f4da051
Adam Gleave <adam@gleave.me>
2019-11-14 16:23:18 -0800

    Migrate and clean-up website JSON generation script

---
+++ src/aprl/visualize/generate_website_json.py
@@ -0,0 +1,113 @@
+from collections import OrderedDict
+import functools
+import json
+import os
+import re
+import sys
+from typing import Any, Iterable, Sequence, Tuple
+
+import boto3
+
+from aprl.visualize import util
+
+ENV_NAME_LOOKUP = {
+    "KickAndDefend-v0": "Kick and Defend",
+    "SumoHumans-v0": "Sumo Humans",
+    "YouShallNotPassHumans-v0": "You Shall Not Pass",
+    "SumoAnts-v0": "Sumo Ants"
+}
+BUCKET_NAME = 'adversarial-policies-public'
+
+
+class NestedDict(OrderedDict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
+
+
+def get_s3_files() -> Iterable[str]:
+    s3 = boto3.resource('s3')
+    adv_policies_bucket = s3.Bucket(BUCKET_NAME)
+    objs = adv_policies_bucket.objects.all()
+    return [os.path.basename(o.key) for o in objs]
+
+
+def get_videos(video_files: Iterable[str]) -> NestedDict:
+    video_files = [path for path in video_files if path.endswith('.mp4')]
+    stem_pattern = re.compile(r'(.*)_[0-9]+p.mp4')
+    agent_pattern = re.compile(r'(\w*-v\d)_victim_(.*)_opponent_(.*)')
+
+    nested = NestedDict()
+    for path in video_files:
+        stem_match = stem_pattern.match(path)
+        if stem_match is None:
+            print("No pattern match found for {}".format(path))
+            continue
+
+        stem = stem_match.groups()[0]
+        assert isinstance(stem, str)
+
+        agent_match = agent_pattern.match(stem)
+        if agent_match is None:
+            print("No pattern match found for {}".format(path))
+            continue
+
+        env_name, victim_abbrev, opponent_abbrev = agent_match.groups()
+        env_name = ENV_NAME_LOOKUP.get(env_name)
+        victim = f'{util.friendly_agent_label(victim_abbrev)} ({victim_abbrev})'
+        opponent = f'{util.friendly_agent_label(opponent_abbrev)} ({opponent_abbrev})'
+        nested[env_name][opponent][victim] = stem
+
+    return nested
+
+
+def sort_fn(item: Tuple[str, Any], groups: Sequence[str]) -> str:
+    """Prepends index of key in groups: can sort in order of groups with alphabetical sort.
+
+    :param item: key-value pair.
+    :param groups: sequence of regexps."""
+    k, v = item
+    match = re.match(r'.* \((.*)\)', k)
+    assert match
+    abbrev = match.groups()[0]
+    for i, grp in enumerate(groups):
+        if re.match(grp, abbrev):
+            break
+    return f'{i}{abbrev}'
+
+
+def sort_nested(nested: NestedDict) -> NestedDict:
+    new_nested = NestedDict()
+    victim_sort = functools.partial(sort_fn, groups=util.GROUPS['rows'])
+    opponent_sort = functools.partial(sort_fn, groups=util.GROUPS['cols'])
+
+    for env, d1 in nested.items():
+        new_d1 = {}
+        for opponent, d2 in d1.items():
+            new_d1[opponent] = OrderedDict(sorted(d2.items(), key=victim_sort))
+        new_nested[env] = OrderedDict(sorted(new_d1.items(), key=opponent_sort))
+    return new_nested
+
+
+def main():
+    paths = get_s3_files()
+    paths = os.listdir('data/videos/defense/20191114_160348/')
+    nested = get_videos(paths)
+    nested = sort_nested(nested)
+
+    out_path = "file_list.json"
+    if len(sys.argv) > 1:
+        out_path = sys.argv[1]
+
+    print(nested)
+    with open(out_path, "w") as fp:
+        json.dump(nested, fp, indent=4)
+    print(f"Saved files to '{out_path}'.")
+
+
+if __name__ == '__main__':
+    main()
